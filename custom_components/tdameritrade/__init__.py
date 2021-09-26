663a2e0b5b63152544f19e582a43ba07c2b27178
PrairieSnpr <midnryanmiller@gmail.com>
2021-09-26 16:09:52 -0400

    Update not_loaded to use ConfigEntryState

--- custom_components/tdameritrade/__init__.py
+++ custom_components/tdameritrade/__init__.py
@@ -4,7 +4,7 @@ import logging
 
 import voluptuous as vol
 
-from homeassistant.config_entries import ConfigEntry
+from homeassistant.config_entries import ConfigEntry, ConfigEntryState
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import (
     aiohttp_client,
@@ -130,7 +130,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass_data["unsub"] = entry.add_update_listener(options_update_listener)
     hass_data["client"] = AmeritradeAPI(auth)
     hass.data[DOMAIN][entry.entry_id] = hass_data
-    if entry.state == "not_loaded":
+    if entry.state == ConfigEntryState.NOT_LOADED:
         for component in PLATFORMS:
             _LOGGER.debug("Setting up %s component", component)
             hass.async_create_task(
